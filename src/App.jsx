94d966312271152eef26c3465ad0b7e9626cc09b
Денис Шагимарданов <denisshagimardanov@MacBook-Pro-Denis.local>
2024-07-04 02:17:19 +0500

    feat: add navigation, install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header.jsx';
 
-import './App.css'
-import Header from "./components/Header/Header.jsx";
+const App = () => {
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </Router>
+    );
+};
 
-function App() {
+// Примеры простых компонентов страниц
+const Home = () => <div>Home Page</div>;
+const About = () => <div>About Page</div>;
+const Services = () => <div>Services Page</div>;
+const Contact = () => <div>Contact Page</div>;
 
-  return (
-    <>
-      <Header/>
-    </>
-  )
-}
-
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
-import './Header.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import searchIcon from '../../assets/search-icon.svg';
+import favoritesIcon from '../../assets/favorites-icon.svg';
+import cartIcon from '../../assets/cart-icon.svg';
+import './Header.css'; // Подключение стилей для хедера
+
 const Header = () => {
     return (
-        <>
-            <header className='header'>
-                <div className="header-sale">
-                    <p className="header-sale__text">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
-                    <a href="#" className="header-sale__link">ShopNow</a>
+        <header className="header">
+            <div className="logo-container">
+                <Link to="/">
+                    <img src={logo} alt="Logo" className="logo" />
+                </Link>
+            </div>
+            <nav className="nav">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/services">Services</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </nav>
+            <div className="search-container">
+                <div className="search-input-container">
+                    <img src={searchIcon} alt="Search" className="search-icon" />
+                    <input type="text" placeholder="What are you looking for?" className="search-input" />
                 </div>
-                <nav className="header-navigation">
-                    <span className="header-navigation-logo"></span>
-                    <ul className="header-navigation-list">
-                        <li className="header-navigation-item">
-                            <a href="#" className="header-navigationitem__link">Cont</a>
-                        </li>
-                        <li className="header-navigation-item">
-                            <a href="#" className="header-navigationitem__link">Cont</a>
-                        </li>
-                        <li className="header-navigation-item">
-                            <a href="#" className="header-navigationitem__link">Cont</a>
-                        </li>
-                        <li className="header-navigation-item">
-                            <a href="#" className="header-navigationitem__link">Cont</a>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </>
+                <button className="icon-button">
+                    <img src={favoritesIcon} alt="Favorites" className="icon" />
+                </button>
+                <button className="icon-button">
+                    <img src={cartIcon} alt="Cart" className="icon" />
+                </button>
+            </div>
+        </header>
     );
 };
 
